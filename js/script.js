5e74ac736f8234a4adcd82f48162fc3546c75e62
William Graham <w.m.graham24@gmail.com>
2017-12-18 20:03:13 +0000

    added time display and scoreboard

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(() => {
-  
+
+  const $startBtn = $('#start-button');
+  const $screen = $('.screen');
+  const colorArray = ['red', 'blue', 'yellow', 'green', 'orange', 'purple'];
+  let lives = 3;
+  let score = null;
+
+  function runGame(){
+    setScreen();
+    colorWord();
+  }
+
+  function setScreen(){
+    const screenWord = colorArray[Math.floor(Math.random()*colorArray.length)];
+    return $screen.text(screenWord);
+  }
+
+  function colorWord(){
+    const fontColor = colorArray[Math.floor(Math.random()*colorArray.length)];
+    return $screen.css('color', `${fontColor}`);
+  }
+
+  $startBtn.on('click', runGame);
 });
