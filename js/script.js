2f277341a635f3b8b2cfc967ef69f455897c1e0a
William Graham <w.m.graham24@gmail.com>
2017-12-18 20:42:43 +0000

    Added score counting logic

--- js/script.js
+++ js/script.js
@@ -2,13 +2,20 @@ $(() => {
 
   const $startBtn = $('#start-button');
   const $screen = $('.screen');
+  const $buttons = $('.color-buttons');
   const colorArray = ['red', 'blue', 'yellow', 'green', 'orange', 'purple'];
   let lives = 3;
   let score = null;
+  let fontColor = null;
 
   function runGame(){
-    setScreen();
-    colorWord();
+    if (lives <= 0) {
+      console.log('Game Over');
+    } else {
+      setScreen();
+      colorWord();
+      activateButtons();
+    }
   }
 
   function setScreen(){
@@ -17,8 +24,26 @@ $(() => {
   }
 
   function colorWord(){
-    const fontColor = colorArray[Math.floor(Math.random()*colorArray.length)];
+    fontColor = colorArray[Math.floor(Math.random()*colorArray.length)];
     return $screen.css('color', `${fontColor}`);
+  }
+
+  function activateButtons(){
+    return $buttons.on('click', checkAnswer);
+  }
+
+  function checkAnswer(e){
+    $buttons.off('click', checkAnswer);
+    const answer = $(e.target).attr('id');
+    if (answer === fontColor){
+      score += 10;
+      console.log(`Score: ${score}`);
+      return runGame();
+    } else {
+      lives -= 1;
+      console.log(`Lives: ${lives}`);
+      return runGame();
+    }
   }
 
   $startBtn.on('click', runGame);
