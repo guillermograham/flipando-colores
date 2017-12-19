9721c6ff6846d2e717ad315615ee97fc82fcf094
William Graham <w.m.graham24@gmail.com>
2017-12-19 11:55:09 +0000

    removed stock button effects

--- js/script.js
+++ js/script.js
@@ -9,23 +9,39 @@ $(() => {
   const $timeRemaining = $('.time-display');
   const colorArray = ['red', 'blue', 'yellow', 'green', 'orange', 'purple'];
   let lives = 3;
-  let score = null;
+  let score = 0;
   let fontColor = null;
   let clockTimer;
   let seconds = 5;
 
   function runGame(){
+    $startBtn.off('click', runGame);
+    $scoreDisplay.text(score);
     $timeRemaining.text(seconds);
     $livesRemaining.text(lives);
     if (lives <= 0) {
       $screen.css('color', 'black');
       $screen.text('Game Over');
+      resetGame();
+      resetClock();
     } else {
       startCountdownTimer();
       setScreen();
       colorWord();
       activateButtons();
     }
+  }
+
+  function resetGame(){
+    lives = 3;
+    score = 0;
+    fontColor = null;
+    $startBtn.on('click', runGame);
+  }
+
+  function resetClock(){
+    clockTimer = null;
+    seconds = 5;
   }
 
   function setScreen(){
@@ -43,15 +59,18 @@ $(() => {
   }
 
   function checkAnswer(e){
+    clearInterval(clockTimer);
     $buttons.off('click', checkAnswer);
     const answer = $(e.target).attr('id');
     if (answer === fontColor){
       score += 10;
       $scoreDisplay.text(score);
+      resetClock();
       return runGame();
     } else {
       lives -= 1;
       $livesRemaining.text(lives);
+      resetClock();
       return runGame();
     }
   }
@@ -64,9 +83,10 @@ $(() => {
     seconds--;
     $timeRemaining.text(seconds);
     if (seconds === 0){
+      $buttons.off('click', checkAnswer);
       clearInterval(clockTimer);
       lives -=1;
-      seconds = 5;
+      resetClock();
       return runGame();
     }
   }
